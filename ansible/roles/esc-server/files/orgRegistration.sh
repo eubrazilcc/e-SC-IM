675eca8b21331607d2108921fc8a09f51ce17e39
Jacek <jacek.cala@gmail.com>
2015-07-10 09:39:48 +0100

    Merge with the master from the e-SC repo

--- ansible/roles/esc-server/files/orgRegistration.sh
+++ ansible/roles/esc-server/files/orgRegistration.sh
@@ -1,21 +1,23 @@
 #!/bin/bash
 
 SERVER_ADDR="localhost"
-ESC_URL=`curl -s --head http://$SERVER_ADDR:8080/beta/ | grep "Location: "`
 FORM_ARGS=`cat $1`
 
-if [[ $ESC_URL == *beta/pages/admin/createorganisation.jsp* ]] ; then
+# Get the last location where the user gets redirected
+ESC_URL=`curl -sL --head http://$SERVER_ADDR:8080/beta/ | grep "Location: " | tail -n 1`
+
+if [[ $ESC_URL == */beta/unsecured/admin/createorganisation.jsp* ]] ; then
     # Redirect to the create organisation page. Let's create the organisation.
-    curl $FORM_ARGS http://$SERVER_ADDR:8080/beta/pages/admin/create.jsp
+    curl $FORM_ARGS http://$SERVER_ADDR:8080/beta/unsecured/admin/create.jsp
 
     # Check if the registration has been successful
     ESC_URL=`curl -s --head http://$SERVER_ADDR:8080/beta/ | grep "Location: "`
-    if [[ $ESC_URL != *beta/pages/front/welcome.jsp* ]]; then
+    if [[ $ESC_URL != *beta/pages/front/index.jsp* ]]; then
         echo "Organisation registration has failed; e-SC URL = $ESC_URL"
         # Somethings wrong, no login screen and no create organisation.
         exit 2
     fi
-elif [[ $ESC_URL != *beta/pages/front/welcome.jsp* ]]; then
+elif [[ $ESC_URL != *beta/pages/front/index.jsp* ]]; then
     # Somethings wrong, no login screen and no create organisation.
     echo "No login screen recognised; e-SC URL = $ESC_URL"
     exit 1
